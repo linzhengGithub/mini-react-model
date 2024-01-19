d674f85f82b64529c33eb940ad9b47ad28d1c37e
linzheng <alidlg9045@126.com>
2024-01-19 23:45:52 +0800

    实现最简 mini-react dom 渲染

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log('mainjs');
+// v1
+// const app = document.createElement('div')
+// app.id = 'app'
+// document.querySelector('#root').append(app)
+
+// const textNode = document.createTextNode('')
+// textNode.nodeValue = 'app'
+// app.append(textNode)
+
+// v2 vdom -> js object
+// const textEl = {
+//   type: 'TEXT_ELEMENT',
+//   props: {
+//     nodeValue: 'app',
+//     children: []
+//   }
+// }
+// const el = {
+//   type: 'div',
+//   props: {
+//     id: 'app',
+//     children: [textEl]
+//   }
+// }
+
+import ReactDom from './core/ReactDom.js';
+import App from './App.js';
+
+ReactDom.createRoot(document.querySelector('#root')).render(App)
