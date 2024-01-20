4e4e308eff19d34d6660445a2c8eb77397424566
linzheng <alidlg9045@126.com>
2024-01-20 19:21:24 +0800

    基于vite 使用jsx

---
+++ vite-runner/main.jsx
@@ -0,0 +1,29 @@
+// v1
+// const app = document.createElement('div')
+// app.id = 'app'
+// document.querySelector('#root').append(app)
+
+// const textNode = document.createTextNode('')
+// textNode.nodeValue = 'app'
+// app.append(textNode)
+
+// v2 vdom -> js object
+// const textEl = {
+//   type: 'TEXT_ELEMENT',
+//   props: {
+//     nodeValue: 'app',
+//     children: []
+//   }
+// }
+// const el = {
+//   type: 'div',
+//   props: {
+//     id: 'app',
+//     children: [textEl]
+//   }
+// }
+
+import ReactDom from './core/ReactDom.js';
+import App from './App.jsx';
+
+ReactDom.createRoot(document.querySelector('#root')).render(App)
